831328243b7ede1fec5053ce1bc7340c18c888a9
josekouma@gmail.com <josekouma@gmail.com>
2024-11-15 15:52:48 -0500

    working on registration page

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,12 @@
 import LoginPanel from "./components/Login/Login"
-import Register from './components/Register/Register';
+import RegisterPanel from './components/Register/Register';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
         <Route path="/login" element={<LoginPanel />} />
-        <Route path="/register" element={Register} />  
+        <Route path="/register" element={<RegisterPanel />} />  
     </Routes>
   );
 }
