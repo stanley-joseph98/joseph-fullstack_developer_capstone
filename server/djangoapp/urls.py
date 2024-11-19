f1422ea086474ab88fb716bdb356361bea5670d3
Joseph Ouma <josekouma@gmail.com>
2024-11-19 15:58:07 -0500

    Dealers dynamic front-end pages by react

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ from .restapis import get_request, analyze_review_sentiments, post_review
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path(route='registration', view=views.registration, name='registration'),
+    path(route='registration/', view=views.registration, name='registration'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
@@ -17,21 +17,21 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     #path for accessing cars, models and makes
-    path(route="get_cars", view=views.get_cars, name='getcars')
+    path(route="get_cars", view=views.get_cars, name='getcars'),
 
     #path for getting all dealerships/dealers
-    path(route="get_dealers", view=views.get_dealerships, name="get_dealers")
+    path(route="get_dealers/", view=views.get_dealerships, name="get_dealers"),
 
     #path for getting dealers by states
     path(route="get_dealers/<str:state>", view=views.get_dealerships, name="get_dealers_by_state"),
 
     #path for getting dealers by id
-    path(route="dealer/<int:dealer_id>",view=views.get_dealer_details, name="dealer_details")
+    path(route="dealer/<int:dealer_id>",view=views.get_dealer_details, name="dealer_details"),
 
     # path for dealer reviews view
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
-    path(route="add_review", view=views.add_review, name="add_review")
+    path(route="add_review", view=views.add_review, name="add_review"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,14 @@
 import LoginPanel from "./components/Login/Login"
-import RegisterPanel from './components/Register/Register';
+import Register from './components/Register/Register';
+import Dealers from './components/Dealers/Dealers';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
         <Route path="/login" element={<LoginPanel />} />
-        <Route path="/register" element={<RegisterPanel />} />  
+        <Route path="/register" element={<Register />} />
+        <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
